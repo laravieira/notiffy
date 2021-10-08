6802c8c3780ce75a6e129d491cf0002f748de107
L4R4V121R4 <contato@laravieira.me>
2021-10-08 06:42:55 -0300

    Add favicon to pages.

--- src/Notiffy/views/message.blade.php
+++ src/Notiffy/views/message.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://www.google.com/recaptcha/enterprise.js"></script>
+    <link rel="shortcut icon" href="https://notiffy.laravieira.me/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://notiffy.laravieira.me/assets/notiffy.css">
     <title>Notiffy</title>
 </head>
